df94f5bce33df78c346175be8d3901cbf46d4f5f
jrgerodriguez <menjivar.jorge.93@gmail.com>
2025-04-27 00:46:28 -0600

    backend created

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
